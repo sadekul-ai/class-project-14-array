b1e5da8bfcc9cfaeaaafc25efb31c40bec13dd7b
sadekul-ai <sadekul.psl@gmail.com>
2025-07-11 11:23:01 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,73 @@
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+//console.log(students);
+
+var numbers = [11, 14, 50, 30, 20, 50];
+//console.log(numbers);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var numberOfStudents = students.length;
+//console.log(numberOfStudents);
+
+var oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+//console.log(oddNumbers.length);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var index = students.indexOf("Abir")
+//console.log(index);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var index = students.indexOf("Rana")
+//console.log(index);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var index = students.indexOf("rana")
+//console.log(index);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var searchPositionBySerialNumber = students[3];
+//console.log(searchPositionBySerialNumber);
+
+var oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+var searchPositionBySerialNumber = oddNumbers[2];
+//console.log(searchPositionBySerialNumber);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+var searchPositionBySerialNumber = students[13];
+//console.log(searchPositionBySerialNumber);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+students [2] = "Fahim Ahmed"
+//console.log(students);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+//console.log(students);
+students [2] = "Fahim Ahmed";
+//console.log(students);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+students.push("Rasel");
+students.push("Sayem");
+//console.log(students);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+students.pop();
+//console.log(students);
+
+var students = ["Sadekul", "Abir", 'Fahim', 'Rana', 15, true];
+students.pop();
+students.pop();
+//console.log(students);
+
+//https://www.w3schools.com/js/js_array_methods.asp
+
+//how to add and remove at begaining and middle
+
+
+
+ 
+
+
+
+
+
